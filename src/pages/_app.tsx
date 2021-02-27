bc195721ba08f575e074df3ea0dea710fed8e9f5
Poosarn Moolmuang <phumoonlight@hotmail.com>
2021-02-28 00:39:40 +0700

    add react strict mode

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,11 @@
 import 'src/global.css'
+import { StrictMode } from 'react'
 import { AppProps } from 'next/app'
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => (
-  <Component {...pageProps} />
+  <StrictMode>
+    <Component {...pageProps} />
+  </StrictMode>
 )
 
 export default App
