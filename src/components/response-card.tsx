07f52688119b7b5f7ad530e3226b39c09056179a
Poosarn Moolmuang <phumoonlight@hotmail.com>
2021-02-28 01:31:34 +0700

    feat: history page

--- src/components/response-card.tsx
+++ src/components/response-card.tsx
@@ -2,9 +2,10 @@ interface Props {
   url: string
   method: string
   data: any
+  createdDate?: string
 }
 
-export const ResponseCard: React.FC<Props> = ({ url, data, method }) => (
+export const ResponseCard: React.FC<Props> = ({ url, data, method, createdDate }) => (
   <div className="bg-white text-white p-4 rounded-lg m-4">
     <div className="bg-black p-4 flex justify-between items-center overflow-x-auto">
       <pre>
@@ -12,6 +13,7 @@ export const ResponseCard: React.FC<Props> = ({ url, data, method }) => (
         <span> </span>
         <span>{url}</span>
       </pre>
+      {createdDate && <pre className="ml-4">{createdDate}</pre>}
     </div>
     <div className="text-xs bg-gray-900 overflow-x-auto p-4">
       <pre>{JSON.stringify(data, null, 2)}</pre>
--- src/pages/history.tsx
+++ src/pages/history.tsx
@@ -9,24 +9,14 @@ import { ResponseCard } from 'src/components/response-card'
 import { ExternalLink } from 'src/components/external-link'
 import { Footer } from 'src/components/footer'
 import { useRequest } from 'src/core/request'
+import { useRequestHistory } from 'src/core/request-history'
 
-const Index: React.FC = () => {
+const History: React.FC = () => {
   const router = useRouter()
   const inputRef = useRef<HTMLInputElement>(null)
   const request = useRequest()
+  const requestHistory = useRequestHistory()
   const queryUrl = router.query.url as string
-
-  const onClickSend = () => {
-    const inputValue = inputRef.current.value
-    const isInputEmpty = !inputValue
-    if (isInputEmpty) return
-    router.push({
-      pathname: '/',
-      query: {
-        url: inputValue,
-      },
-    })
-  }
 
   useEffect(() => {
     if (!queryUrl) return
@@ -37,7 +27,7 @@ const Index: React.FC = () => {
   return (
     <>
       <Head>
-        <title>Fetcher</title>
+        <title>History | Fetcher</title>
       </Head>
       <PageLayout
         className="bg-gradient-to-r from-green-500 to-blue-500"
@@ -45,7 +35,7 @@ const Index: React.FC = () => {
           <header>
             <nav className="p-6 flex flex-col md:flex-row justify-between">
               <div className="text-4xl text-white font-bold mb-4 md:mb-0">
-                <Link href="/">{'Fetcher { }'}</Link>
+                <Link href="/">{'Fetcher { } | History'}</Link>
               </div>
               <div className="flex">
                 <div className="font-bold text-white grid grid-cols-2 gap-4 opacity-90">
@@ -60,33 +50,16 @@ const Index: React.FC = () => {
           </header>
         }
         main={
-          <main>
-            <section className="grid grid-cols-12 gap-4 m-4">
-              <input
-                className="col-span-12 md:col-span-10 p-3 font-mono text-2xl rounded-xl w-full transition-shadow shadow-xl focus:shadow-2xl focus:outline-none"
-                type="text"
-                placeholder="URL"
-                ref={inputRef}
+          <main className="flex flex-col-reverse">
+            {requestHistory.list.map((history) => (
+              <ResponseCard
+                method="GET"
+                key={history.createdDate}
+                data={history.data}
+                url={history.url}
+                createdDate={history.createdDate}
               />
-              <button
-                className="col-span-12 md:col-span-2 p-3 uppercase rounded-xl w-full transition-all shadow-xl focus:shadow-2xl bg-green-500 hover:bg-green-400 font-bold tracking-widest focus:outline-none text-2xl text-white"
-                onClick={onClickSend}
-                disabled={request.isLoading}
-                children={request.isLoading ? 'sending...' : 'send'}
-              />
-            </section>
-            <section>
-              {request.isRequestError && (
-                <ErrorRequestCard error={request.error} />
-              )}
-              {request.responseData && (
-                <ResponseCard
-                  data={request.responseData}
-                  method={request.method}
-                  url={queryUrl}
-                />
-              )}
-            </section>
+            ))}
           </main>
         }
         footer={<Footer />}
@@ -95,4 +68,4 @@ const Index: React.FC = () => {
   )
 }
 
-export default Index
+export default History
